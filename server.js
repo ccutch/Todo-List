cfa7e50b945f20d60afe2ce01c954fd7bf4eb84f
Connor McCutcheon <connor@nyentek.com>
2015-03-02 22:41:50 -0800

    launching browser for ease of use

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var open = require('open'); 
+
 connect().use(serveStatic(__dirname)).listen(1234);
+console.log('Listening on port 1234');
+
+open('http://127.0.0.1:1234');
